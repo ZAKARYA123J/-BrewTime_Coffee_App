d24edb8ba72c9ef8fb86d83e604a7ae48a80a328
zakaria <zakaryabaouali255@gmail.com>
2025-10-23 14:47:45 +0100

    change

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,57 +1,14 @@
-// import { Tabs } from 'expo-router';
-// import React from 'react';
-
-<<<<<<< HEAD
-// import { HapticTab } from '@/components/haptic-tab';
-// import { IconSymbol } from '@/components/ui/icon-symbol';
-// import { Colors } from '@/constants/theme';
-// import { useColorScheme } from '@/hooks/use-color-scheme';
-=======
+import { Tabs } from 'expo-router';
+import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Ionicons } from '@expo/vector-icons';
->>>>>>> 7b2061422d769bdab007c79f5efa91d9ec94a89f
 
-// export default function TabLayout() {
-//   const colorScheme = useColorScheme();
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
 
-<<<<<<< HEAD
-//   return (
-//     <Tabs
-//       screenOptions={{
-//         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-//         headerShown: false,
-//         tabBarButton: HapticTab,
-//       }}>
-//       <Tabs.Screen
-//         name="index"
-//         options={{
-//           title: 'Home',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="explore"
-//         options={{
-//           title: 'Explore',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="profile"
-//         options={{
-//           title: 'Explore',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-//         }}
-//       />
-    
-//     </Tabs>
-//   );
-// }
-=======
   return (
     <Tabs
       screenOptions={{
@@ -69,18 +26,18 @@ import { Ionicons } from '@expo/vector-icons';
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Menu',
-          tabBarIcon: ({ color }) => <Ionicons name="menu" size={28} color={color} />,
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="login"
+        name="profile"
         options={{
-          title: 'Login',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="person-circle" color={color} />,
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+    
     </Tabs>
   );
 }
->>>>>>> 7b2061422d769bdab007c79f5efa91d9ec94a89f
